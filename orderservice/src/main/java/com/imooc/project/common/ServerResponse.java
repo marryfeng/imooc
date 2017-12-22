dd5716568375b3c78136f7a3a301a7bcfb1d21bd
fengli <1473357859@qq.com>
2017-12-22 19:45:30 +0800

    订单功能模块

---
+++ orderservice/src/main/java/com/imooc/project/common/ServerResponse.java
@@ -0,0 +1,84 @@
+package com.imooc.project.common;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.Serializable;
+
+/**
+ * 服务端的返回响应对象
+ */
+//当程序返回结果失败的时候，此时data是null，这个时候我们不需要返回data的null，利用这个NON_NULL，我们就不返回空的data
+@JsonSerialize(include =  JsonSerialize.Inclusion.NON_NULL)
+public class ServerResponse<T> implements Serializable {
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status) {
+        this.status = status;
+    }
+
+    private ServerResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+//这里需要注意的是，如果将private改为public时，是传递给T data呢还是传递给String msg呢，这是需要注意的
+    private ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    private ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+    @JsonIgnore
+    public boolean isSuccess(){
+        return this.status==ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(String msg,T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+
+
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+
+
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
+        return new ServerResponse<T>(errorCode,errorMessage);
+    }
+}
